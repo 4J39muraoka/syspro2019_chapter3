d6a012cb63106804388ec723babcd6b196e18979
4J39muraoka <s16183@tokyo.kosen-ac.jp>
2019-09-26 02:25:20 +0100

    1秒周期の点滅のプログラムの追加

--- ledon.py
+++ ledon.py
@@ -4,10 +4,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
-for i in range(10):
-    GPIO.output(14, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(14, GPIO.LOW)
-    time.sleep(0.5)
+GPIO.output(14, GPIO.HIGH)
+time.sleep(10)
 
 GPIO.cleanup()
